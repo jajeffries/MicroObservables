f29887d99dfe14540e1516bfdf7be3b2580765bf
James Jeffries <jamesjeffries1@gmail.com>
2014-01-13 13:21:54 +0000

    Added minification

--- GruntFile.js
+++ GruntFile.js
@@ -11,13 +11,25 @@ module.exports = function(grunt) {
  		},
  		qunit : {
 			all: ['test/**/*.html']
- 		}
+ 		}, 
+ 		uglify : {
+ 			options: {
+ 				report: 'gzip'
+ 			},
+ 			min: {
+	 			files: {
+	 				'microObservables.min.js' : 'microObservables.js'
+	 			}
+	 		}
+ 		} 
  	});
 
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 
+	grunt.registerTask('min', ['uglify']);
 	grunt.registerTask('test', ['jshint', 'qunit']);
-	grunt.registerTask('default', ['jshint', 'qunit']);
+	grunt.registerTask('default', ['test', 'min']);
 
 };
